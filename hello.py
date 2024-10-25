a687aa64f15a69620f275b0884c69aa94534cc9d
Wyj7835 <1791146173@qq.com>
2024-10-25 20:37:55 +0800

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,6 @@
+print("Hello from Binder!");
+
+<!---
+Wyj7835/Wyj7835 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
